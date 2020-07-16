eaa26fe34f90273e92434a7bddc68b158dc4060e
always0ne <si8363@naver.com>
2020-07-16 13:37:57 +0900

    feat: JWT Login
    ## 구현된 기능
    - signIn
    - singup
    - 패스워드 암호화
    - 테스트 코드
    
    ## 사용법
    - 정상적으로 로그인한다.
    - 로그인할때 받은 토큰을 Authorization 이름의 헤더에 `Bearer Token`을 실어서 서비스를 이용한다.
    
    Resolves: #3
    See also: #-

---
+++ src/config/passport.js
@@ -0,0 +1,55 @@
+import userRepo from "../repositories/user";
+import bcrypt from "bcrypt";
+
+const passport = require('passport');
+const passportJWT = require('passport-jwt');
+const JWTStrategy   = passportJWT.Strategy;
+const ExtractJWT = passportJWT.ExtractJwt;
+const LocalStrategy = require('passport-local').Strategy;
+let UserModel = require('../models').User;
+require('dotenv').config();
+module.exports = () => {
+    // Local Strategy
+    passport.use(new LocalStrategy({
+            usernameField: 'nickname',
+            passwordField: 'password'
+        },
+        function (nickname, password, done) {
+            // 이 부분에선 저장되어 있는 User를 비교하면 된다.
+            return userRepo.findByNickname(nickname)
+                .then(user => {
+                    if (!user) {
+                        return done(null, false, {message: 'Incorrect ID or password.'});
+                    }
+                    else{
+                        bcrypt.compare(password, user.password)
+                            .then(pwCheck =>{
+                                console.log(pwCheck)
+                                if(!pwCheck)
+                                    return done(null, false, {message: 'Incorrect ID or password.'});
+                                else
+                                    return done(null, user, {message: 'Logged In Successfully'});
+                            })
+                            .catch(err => done(err));
+                    }
+                })
+                .catch(err => done(err));
+        }
+    ));
+
+    //JWT Strategy
+    passport.use(new JWTStrategy({
+            jwtFromRequest: ExtractJWT.fromAuthHeaderAsBearerToken(),
+            secretOrKey   : process.env.JWT_SECRET
+        },
+        function (jwtPayload, done) {
+            return userRepo.findByNickname(jwtPayload.nickname)
+                .then(user => {
+                    return done(null, user);
+                })
+                .catch(err => {
+                    return done(err);
+                });
+        }
+    ));
+};
