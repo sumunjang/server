e47f4bb052c3436d371af94fb4072058c5cd01b3
always0ne <si8363@naver.com>
2020-07-21 03:38:54 +0900

    refactor: 깨진 테스트 수정
    로그인 수정

--- src/services/authService.js
+++ src/services/authService.js
@@ -18,7 +18,6 @@ exports.signIn = function (req, res) {
             if (err) {
                 res.send(err);
             }
-            // jwt.sign('token내용', 'JWT secretkey')
             const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET);
             const username = user.name;
             return res.json({username, token});
@@ -31,26 +30,5 @@ exports.signUp = async (req, res, next) => {
         user_id: req.body.userid,
         password: req.body.password,
         name: req.body.name
-    }).then(
-        passport.authenticate('local', {session: false}, (err, user) => {
-            if (err) {
-                return res.status(500).json({
-                    message: 'Something is wrong.'
-                });
-            }
-            if(!user)
-                return res.status(400).json({
-                    message: 'Incorrect ID or password.'
-                });
-            req.login(user, {session: false}, (err) => {
-                if (err) {
-                    res.send(err);
-                }
-                // jwt.sign('token내용', 'JWT secretkey')
-                const token = jwt.sign(user.toJSON(), process.env.JWT_SECRET);
-                const username = user.name;
-                return res.json({username, token});
-            });
-        })(req, res)
-    )
+    }).then(res.json())
 };
