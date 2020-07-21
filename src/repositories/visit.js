f90cb62c334bc0fda5da5687bfe95321690e456a
always0ne <si8363@naver.com>
2020-07-22 00:44:06 +0900

    refactor: db 테이블 변경으로 인한 repository 변경

--- src/repositories/visit.js
+++ src/repositories/visit.js
@@ -9,23 +9,29 @@ export default {
 
     findById: async (id) => await models.visit.findByPk(id),
     findByuserId: async (userId) => await models.visit.findOne({
+        where:{
+            userId:userId
+        },
          include:[
-            {model: models.User, required:true, where:{user_id:userId}},
             {model: models.Place, required:true}
             ]
     }),
     findAllByuserId: async (size, userId) => await models.visit.findAll({
         limit: size,
+        where:{
+          userId:userId
+        },
         include:[
-            {model: models.User, required:true, where:{user_id:userId}},
             {model: models.Place, required:true}
         ]
     }),
     findLatestOneByuserId: async (userId) => await models.visit.findAll({
     limit: 1,
         order:[['createdAt','DESC']],
+        where:{
+            userId:userId
+        },
     include:[
-        {model: models.User, required:true, where:{user_id:userId}},
         {model: models.Place, required:true}
     ]
 }),
