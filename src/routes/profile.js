29b5348aebcad3ac7ba76b107085f245d24cdce5
always0ne <si8363@naver.com>
2020-07-21 17:54:29 +0900

    refactor: delete image in profile

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -7,8 +7,4 @@ router.get('/', profileService.getProfile);
 router.put('/', profileService.updateProfile);
 router.delete('/', profileService.withdrawal);
 
-router.get('/image', profileService.getProfileImage);
-router.post('/image', profileService.uploadProfileImage);
-router.put('/image', profileService.updateProfileImage);
-
 module.exports = router;
--- src/services/profileService.js
+++ src/services/profileService.js
@@ -30,15 +30,3 @@ exports.updateProfile = async (req, res, next) => {
 exports.withdrawal = async (req, res, next) => {
 
 };
-
-exports.getProfileImage = async (req, res, next) => {
-
-};
-
-exports.uploadProfileImage = async (req, res, next) => {
-
-};
-
-exports.updateProfileImage = async (req, res, next) => {
-
-};
