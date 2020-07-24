294833ead1df51f57f73e9e3290e36b9dd4daa9b
CodingNome <si8363@naver.com>
2020-07-25 05:09:17 +0900

    feat: 방문 리스트

--- src/services/formService.js
+++ src/services/formService.js
@@ -2,9 +2,19 @@ import questionRepo from "../repositories/question";
 import answerRepo from "../repositories/answer";
 import visitRepo from "../repositories/visit"
 import jwt from "jsonwebtoken";
+import place from "../repositories/place";
 
 exports.getMySubmits = async (req, res, next) => {
-    // req.params.keyword;
+    const token = req.headers.authorization.split('Bearer ')[1];
+    const userId = jwt.verify(token, process.env.JWT_SECRET).user_id;
+    visitRepo.findAllDescByuserId(userId)
+        .then( result =>{
+            return result.map(function (value, index) {
+                var submitid = value.id;
+                var placeid = value.Place.id;
+                return {placeid,submitid}
+            })
+        }).then( list => res.json(list) )
 };
 
 exports.getFormInPlace = async (req, res, next) => {
@@ -99,4 +109,28 @@ exports.submitForm = async (req, res, next) => {
 
 exports.updateForm = async (req, res, next) => {
     // req.params.placeid;
+    questionRepo.findAllByPlaceId(req.params.placeid)
+        .then(async result => {
+                for(var i =0;i<req.body.data.length;i++){
+                    var isDeleted = true;
+                    console.log(req.body.data[i])
+                    if(req.body.data[i].questionid>0){
+                        for(var j =0;result[j]!=undefined;j++){
+                            if(req.body.data[i].questionid === result[j].id){
+                                isDeleted=false;
+                                await questionRepo.updateByQuestionId(req.body.data[i].question,result[j].id)
+                            }
+                        }
+                    }else{
+                        isDeleted=false;
+                        var question = req.body.data[i].question;
+                        var placeId = req.params.placeid
+                        await questionRepo.store({id:null,question:question,PlaceId:placeId})
+                    }
+                    if(isDeleted){
+                        await questionRepo.deleteByQuestionId(req.body.data[i].questionid)
+                    }
+                }
+            }
+        ).then(res.json(""))
 };
